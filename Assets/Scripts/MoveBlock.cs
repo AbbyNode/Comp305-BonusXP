71c02c9d0309f20c5aa1b297557af058fd952697
Jupiter1997 <jupiter.jv@gmail.com>
2017-11-29 09:39:07 -0500

    MoveBlockScript Modified

--- Assets/Scripts/MoveBlock.cs
+++ Assets/Scripts/MoveBlock.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveBlock : MonoBehaviour {
-
+    public double maxJump;
+    public double minJump;
+    private Transform currentPos;
+    Rigidbody2D rb;
 	// Use this for initialization
 	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        currentPos = this.transform;
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+       
+        this.rb.gravityScale = 0.5f;
+       
+        if (currentPos.position.y <= minJump)
+        {
+            this.rb.gravityScale = -1;
+       
+        }
+
+    }
 }
