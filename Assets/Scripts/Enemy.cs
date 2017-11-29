2c4f4d4ad15eb884e7236a1468150c0b926d9b6b
deruip <aronly1997@hotmail.com>
2017-11-29 09:41:12 -0500

    Push test

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,15 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour {
+public class Enemy : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float speed;
+    private float savedTime = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        transform.Translate(new Vector3(speed, 0, 0) * Time.deltaTime);
+        if (Time.time - savedTime >= 5)
+        {
+            speed *= -1;
+        }
+        savedTime = Time.time;
+    }
 }
